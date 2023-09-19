94f5e4dfbf32d86dfaf78c57737b825c9ca46ed5
sudeepmahato16 <sudeepmahato16@gmail.com>
2023-09-19 21:22:48 +0545

    Add shadow to categories on scroll

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 interface ContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
+const Container: React.FC<ContainerProps> = ({ children, className }) => {
   return (
-    <div className="max-w-[1140px] mx-auto xl:px-0 px-4">
+    <div className={`max-w-[1140px] mx-auto xl:px-0 px-4 ${className}`}>
       {children}
     </div>
   );
--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
+import "swiper/css";
 
 import Container from "../Container";
 import CategoryBox from "../CategoryBox";
@@ -12,40 +12,53 @@ import { categories } from "@/constants";
 import { Category } from "@/types";
 
 const Categories = () => {
+  const [isActive, setIsActive] = useState(false);
   const params = useSearchParams();
   const pathname = usePathname();
   const category = params?.get("category");
 
   const isMainPage = pathname === "/";
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsActive(true);
+      } else {
+        setIsActive(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   if (!isMainPage) {
     return null;
   }
 
   return (
+    <div className={`${isActive ? "shadow-md shadow-[rgba(0,0,0,.045)]" : ""} transition-all duration-150`}>
     <Container>
-      <Swiper slidesPerView="auto"
+      <Swiper
+        slidesPerView="auto"
         pagination={{
           clickable: true,
-        }} className="w-full mt-2">
+        }}
+        className="w-full mt-2"
+      >
         {categories.map((item: Category) => (
-
           <SwiperSlide className="max-w-fit" key={item.label}>
-              <CategoryBox
-                label={item.label}
-                icon={item.icon}
-                selected={category === item.label}
-              />
+            <CategoryBox
+              label={item.label}
+              icon={item.icon}
+              selected={category === item.label}
+            />
           </SwiperSlide>
-
         ))}
       </Swiper>
     </Container>
+              </div>
   );
 };
 
 export default Categories;
-
-
-
-
